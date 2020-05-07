2481518d9b244b5e7a4702a659868e7364798396
Nik <tshl20141@gmail.com>
2020-05-07 14:30:42 +0300

    Upload

---
+++ js/script.js
@@ -0,0 +1,532 @@
+$(".card-A-0").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/Electronic transformers on discrete half-bridge for halogen lamps 12V/Half-Bridge Convertor PCB_bott.jpg',
+            title : 'Half-Bridge Convertor PCB_bott'
+        },
+        {
+            href : 'img/Analog/Electronic transformers on discrete half-bridge for halogen lamps 12V/Half-Bridge Convertor PCB_top.jpg',
+            title : 'Half-Bridge Convertor PCB_top'
+        },
+        {
+            href : 'img/Analog/Electronic transformers on discrete half-bridge for halogen lamps 12V/Half-Bridge Convertor Schematic.jpg',
+            title : 'Half-Bridge Convertor Schematic'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 115,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-1").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/Electronic transformers on IR2161s for halogen lamps 12V/Half-Bridge Convertor_ir2161s PCB_bott.jpg',
+            title : 'Half-Bridge Convertor_ir2161s PCB_bott'
+        },
+        {
+            href : 'img/Analog/Electronic transformers on IR2161s for halogen lamps 12V/Half-Bridge Convertor_ir2161s PCB_top.jpg',
+            title : 'Half-Bridge Convertor_ir2161s PCB_top'
+        },
+        {
+            href : 'img/Analog/Electronic transformers on IR2161s for halogen lamps 12V/Half-Bridge Convertor_ir2161s Schematic.jpg',
+            title : 'Half-Bridge Convertor_ir2161s Schematic'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-2").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/LED-driver 46W 350mA on UCC28810/LED-driver 46W 350mA on UCC28810_1.jpg',
+            title : 'LED-driver 46W 350mA on UCC28810'
+        },
+        {
+            href : 'img/Analog/LED-driver 46W 350mA on UCC28810/LED-driver 46W 350mA on UCC28810_2.jpg',
+            title : 'LED-driver 46W 350mA on UCC28810'
+        },
+        {
+            href : 'img/Analog/LED-driver 46W 350mA on UCC28810/LED-driver 46W 350mA on UCC28810_3.jpg',
+            title : 'LED-driver 46W 350mA on UCC28810'
+        },
+        {
+        	href : 'img/Analog/LED-driver 46W 350mA on UCC28810/LED-driver 46W 350mA on UCC28810_4.jpg',
+            title : 'LED-driver 46W 350mA on UCC28810'
+        },
+        {
+        	href : 'img/Analog/LED-driver 46W 350mA on UCC28810/LED-driver 46W 350mA on UCC28810_Schematic.jpg',
+            title : 'LED-driver 46W 350mA on UCC28810'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-3").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/LED-driver adjustable on UCC28810/LED-driver adjustable on UCC28810_1.jpg',
+            title : 'LED-driver adjustable on UCC28810'
+        },
+        {
+            href : 'img/Analog/LED-driver adjustable on UCC28810/LED-driver adjustable on UCC28810_2.jpg',
+            title : 'LED-driver adjustable on UCC28810'
+        },
+        {
+            href : 'img/Analog/LED-driver adjustable on UCC28810/LED-driver adjustable on UCC28810_3.jpg',
+            title : 'LED-driver adjustable on UCC28810'
+        },
+        {
+            href : 'img/Analog/LED-driver adjustable on UCC28810/LED-driver adjustable on UCC28810_Schematic.jpg',
+            title : 'LED-driver adjustable on UCC28810'
+        },
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-4").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/Simple ballast for fluorescent lamps T8/Simple ballast for fluorescent lamps T8 PCB3D model.jpg',
+        title : 'Simple ballast for fluorescent lamps T8 PCB3D model'
+        },
+        {
+            href : 'img/Analog/Simple ballast for fluorescent lamps T8/Simple ballast for fluorescent lamps T8_Photo1.jpg',
+            title : 'Simple ballast for fluorescent lamps T8_Photo1'
+        },
+        {
+            href : 'img/Analog/Simple ballast for fluorescent lamps T8/Simple ballast for fluorescent lamps T8 Photo2.jpg',
+            title : 'Simple ballast for fluorescent lamps T8_Photo1'
+        },
+        {
+            href : 'img/Analog/Simple ballast for fluorescent lamps T8/Simple ballast for fluorescent lamps T8 Photo3.jpg',
+            title : 'Simple ballast for fluorescent lamps T8_Photo1'
+        },
+        {
+        		href : 'img/Analog/Simple ballast for fluorescent lamps T8/Simple ballast for fluorescent lamps T8_Schematic.jpg',
+            title : 'Simple ballast for fluorescent lamps T8_Schematic'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-5").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/Simple LED-driver 8W 65mA on HV9961/Simple LED-driver 8W 65mA on HV9961_2.jpg',
+            title : 'Simple LED-driver 8W 65mA on HV9961'
+        },
+        {
+            href : 'img/Analog/Simple LED-driver 8W 65mA on HV9961/Simple LED-driver 8W 65mA on HV9961_1.jpg',
+            title : 'Simple LED-driver 8W 65mA on HV9961'
+        },
+        {
+            href : 'img/Analog/Simple LED-driver 8W 65mA on HV9961/Simple LED-driver 8W 65mA on HV9961_3.jpg',
+            title : 'Simple LED-driver 8W 65mA on HV9961'
+        },
+        {
+        	href : 'img/Analog/Simple LED-driver 8W 65mA on HV9961/Simple LED-driver 8W on HV9961 3DModel.jpg',
+            title : 'Simple LED-driver 8W 65mA on HV9961'
+        },
+        {
+        	href : 'img/Analog/Simple LED-driver 8W 65mA on HV9961/Simple LED-driver 8W 65mA on HV9961_Circuit.jpg',
+            title : 'Simple LED-driver 8W 65mA on HV9961'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-6").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/Simple LED-driver 36W 350mA on BP2857d/Simple LED-driver on BP2857d_3.jpg',
+            title : 'Simple LED-driver on BP2857d'
+        },
+        {
+            href : 'img/Analog/Simple LED-driver 36W 350mA on BP2857d/Simple LED-driver on BP2857d_2.jpg',
+            title : 'Simple LED-driver on BP2857d'
+        },
+        {
+            href : 'img/Analog/Simple LED-driver 36W 350mA on BP2857d/Simple LED-driver on BP2857d_1.jpg',
+            title : 'Simple LED-driver on BP2857d'
+        },
+        {
+            href : 'img/Analog/Simple LED-driver 36W 350mA on BP2857d/LED driver on BP2857d_Schematic.jpg',
+            title : 'Simple LED-driver on BP2857d'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-7").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/Smart ballast for fluorescent lamps T8/Smart ballast for fluorescent lamps T8 PCB_3D model.png',
+            title : 'Smart ballast for fluorescent lamps T8'
+        },
+        {
+            href : 'img/Analog/Smart ballast for fluorescent lamps T8/Smart ballast for fluorescent lamps T8 PCB_bottom.jpg',
+            title : 'Smart ballast for fluorescent lamps T8'
+        },
+        {
+            href : 'img/Analog/Smart ballast for fluorescent lamps T8/Smart ballast for fluorescent lamps T8 PCB_top.jpg',
+            title : 'Smart ballast for fluorescent lamps T8'
+        },
+        {
+        		href : 'img/Analog/Smart ballast for fluorescent lamps T8/Smart ballast for fluorescent lamps T8 Schematic.jpg',
+            title : 'Smart ballast for fluorescent lamps T8'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-A-8").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Analog/DC low-voltage LED-driver on LM3409 for low-profile applications/DSC_3916.jpg',
+            title : 'DC low-voltage LED-driver on LM3409 for low-profile applications'
+        },
+        {
+            href : 'img/Analog/DC low-voltage LED-driver on LM3409 for low-profile applications/DSC_3907.jpg',
+            title : 'DC low-voltage LED-driver on LM3409 for low-profile applications.'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-D-0").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-1/ACS Mellon-1_1.jpg',
+            title : 'ACS Mellon-1'
+        },
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-1/ACS Mellon-1_2.jpg',
+            title : 'ACS Mellon-1'
+        },
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-1/ACS Mellon-1_3.jpg',
+            title : 'ACS Mellon-1'
+        },
+        {
+        	href : 'img/Digital/Access Сontrol System Mellon-1/ACS Mellon-1_4.jpg',
+            title : 'ACS Mellon-1'
+        },
+        {
+        	href : 'img/Digital/Access Сontrol System Mellon-1/ACS Mellon-1_schematic.jpg',
+            title : 'ACS Mellon-1'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-D-1").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-2/Access Сontrol System Mellon-2_1.jpg',
+            title : 'ACS Mellon-2'
+        },
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-2/Access Сontrol System Mellon-2_2.jpg',
+            title : 'ACS Mellon-2'
+        },
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-2/Access Сontrol System Mellon-2_3.jpg',
+            title : 'ACS Mellon-2'
+        },
+        {
+            href : 'img/Digital/Access Сontrol System Mellon-2/Access Сontrol System Mellon-2_schematic.jpg',
+            title : 'ACS Mellon-2'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-D-2").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Digital/Digital thermometer with LCD/Digital thermometer LCD_1.jpg',
+            title : 'Digital thermometer LCD'
+        },
+        {
+            href : 'img/Digital/Digital thermometer with LCD/Digital thermometer with LCD_2.jpg',
+            title : 'Digital thermometer LCD'
+        },
+        {
+            href : 'img/Digital/Digital thermometer with LCD/Digital thermometer with LCD_3.jpg',
+            title : 'Digital thermometer LCD'
+        },
+        {
+        	href : 'img/Digital/Digital thermometer with LCD/Digital thermometer with LCD_schematic.jpg',
+            title : 'Digital thermometer LCD'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-D-3").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Digital/RS485-USB/USB-RS485 Photo_1.jpg',
+            title : 'USB-RS485'
+        },
+        {
+            href : 'img/Digital/RS485-USB/USB-RS485 Photo_2.jpg',
+            title : 'USB-RS485'
+        },
+        {
+            href : 'img/Digital/RS485-USB/USB-RS485 Photo_3.jpg',
+            title : 'USB-RS485'
+        },
+        {
+            href : 'img/Digital/RS485-USB/USB-RS485_schematic.jpg',
+            title : 'USB-RS485'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
+$(".card-D-4").click(function() {
+    $.fancybox.open([
+        {
+            href : 'img/Digital/Smart Control and Security System/Smart Control and Security System_1.jpg',
+            title : 'Smart Control and Security System'
+        },
+        {
+            href : 'img/Digital/Smart Control and Security System/Smart Control and Security System_2.jpg',
+            title : 'Smart Control and Security System'
+        },
+        {
+        	href : 'img/Digital/Smart Control and Security System/Smart Control and Security System_3.jpg',
+            title : 'Smart Control and Security System'
+        },
+        {
+        	href : 'img/Digital/Smart Control and Security System/Smart Control and Security System_4.jpg',
+            title : 'Smart Control and Security System'
+        }
+    ], {
+        nextEffect : 'none',
+        prevEffect : 'none',
+        padding    : 0,
+        helpers    : {
+            title : {
+                type: 'over'  
+            },
+            thumbs : {
+                width  : 75,
+                height : 50,
+                source : function( item ) {
+                    return item.href.replace('../img/thumbs/_back.png', '../img/thumbs/_next.png');
+                }
+            }
+        }
+    });
+    
+    return false;
+});
